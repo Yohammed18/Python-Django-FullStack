f556fe4df78ec8a02e16c0580d52acbce46f2aa4
Mohammed Allibalogun <mallibalogun18@gmail.com>
2024-02-16 23:05:20 -0500

    Django Level Two: Populated records into a table that will display on the index.html page

--- django/first_project/first_app/views.py
+++ django/first_project/first_app/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# connect database by importing models
+from first_app.models import Webpage, Topic, AccessRecord
+
 # Create your views here.
 def index(request):
-    # return HttpResponse("Hello World!")
-    my_dict = {'insert_me':'Now I am coming from first_app/index.html'}
-    return render(request, 'first_app/index.html', context=my_dict)
+    #grab access records
+    webpages_list = AccessRecord.objects.order_by('date')
+    date_dict = {'access_records': webpages_list}
+
+    return render(request, 'first_app/index.html', context=date_dict)
+
+
